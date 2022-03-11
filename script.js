f8e6d42d69540854be838bd476843cae790af716
Shahid Ameen <43674745+iamshahidameen@users.noreply.github.com>
2022-03-12 02:12:21 +0500

    Data get using aync and await to make the code cleaner without using .then

--- script.js
+++ script.js
@@ -4,10 +4,23 @@ const genBtn = document.getElementById('jokeBtn');
 window.addEventListener('load', generateJokes);
 genBtn.addEventListener('click', generateJokes);
 
-function generateJokes(){
-    fetch('https://icanhazdadjoke.com', {
+// function generateJokes(){
+//     fetch('https://icanhazdadjoke.com', {
+//         headers: {
+//             'Accept': 'application/json'
+//     }
+// }).then(res => res.json()).then(data => jokeMain.innerHTML = data.joke)
+// }
+
+async function generateJokes(){
+    const config = {
         headers: {
             'Accept': 'application/json'
     }
-}).then(res => res.json()).then(data => jokeMain.innerHTML = data.joke)
+};
+    const apiRes = await fetch('https://icanhazdadjoke.com',config)
+    
+    const data = await apiRes.json();
+    jokeMain.innerHTML = data.joke
 }
+
