d9e96537fad1e8f555e5e4c2985d57b4763d41b3
Shahid Ameen <43674745+iamshahidameen@users.noreply.github.com>
2022-03-12 01:17:14 +0500

    Usses Fetch API to get the data from Icanhazdadjoke, uses .then method to load the data in our frontend

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("Don't, Laugh");
+const jokeMain = document.getElementById('joke');
+const genBtn = document.getElementById('jokeBtn');
+
+window.addEventListener('load', generateJokes);
+genBtn.addEventListener('click', generateJokes);
+
+function generateJokes(){
+    fetch('https://icanhazdadjoke.com', {
+        headers: {
+            'Accept': 'application/json'
+    }
+}).then(res => res.json()).then(data => jokeMain.innerHTML = data.joke)
+}
